ef02b22fc140419e83cc39a78828e64f420b8b72
DUCKENG <ducanhcktt@gmail.com>
2024-08-01 10:00:38 +0700

    test

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Happy birtday",
+    introTitle: 'DICO <3 ',
+    introDesc: `Bạn sẵn sàng gửi những lời chúc tốt đẹp đến DUCKENG đẹp zai chứ😘😘 `,
+    btnIntro: '^^Đồng ý^^',
+    title: 'Bạn luôn luôn muốn gửi những lời chúc tốt đẹp đến DUCKENG phải không 🥰',
+    desc: 'Hãy thực hiện điều đó ngay bây giờ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Hãy dành những lời chúc thân thương nhất của bạn cho mình nhé <3',
+    btnReply: 'Gửi cho DUCKENG <3',
+    reply: 'DUCKENG là một người bạn tuyệt vời, một vài câu không thể nói hết về bạn <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Nhớ gửi quà sinh nhật qua stk ngân hàng cho mình nhá.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/ducanh070' //link mess của các bạn. VD: https://m.me/ducanh070
+}
